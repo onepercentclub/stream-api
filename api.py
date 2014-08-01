a6076482e80609f1d33da1ee353d49c0b326f043
Marco <marco@onepercentclub.com>
2014-08-01 15:19:30 +0200

    Integration of twitter messages

--- api.py
+++ api.py
@@ -5,36 +5,57 @@ from utils import append_value, path_value
 
 class Root:
     def index(self):
-	    return "Hello, world!"
+        return "Hello, world!"
     index.exposed = True
 
 class Messages(object):
 
     def GET(self, source=None):
-	
-	messages = mongo.get_messages_collection()
-	result = messages.find_one({"source": source})
-	
-	if source == None:
-	    result = "Here you have all the messages: \n"
-	    for message in messages.find():
-	    	#result += mongo.to_string(message)
-		result = message['text']
-		result += "\n ====== \n"
-	
-	elif result:
-	    result = mongo.to_string(message)
-	
-	else:
-	    result = "No messages with the specified ID %s" %source
+        messages = mongo.get_messages_collection()
+        result = messages.find_one({"source": source})
+        
+        if source == None:
+            result = "Here you have all the messages: \n"
+            for message in messages.find():
+                #result += mongo.to_string(message)
+                result = message['text']
+                result += "\n ====== \n"
+        
+        elif result:
+            result = mongo.to_string(message)
+        
+        else:
+            result = "No messages with the specified ID %s" %source
 
-	return result
+        return result
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
     def POST(self):
         result = mongo.post_message(cherrypy.request.json)
         return "Success"    
+
+    exposed = True
+
+class TwitterMentions(object):
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    def POST(self):
+        json = cherrypy.request.json
+        # import ipdb; ipdb.set_trace()
+        message = {}
+
+        if json['user']:
+            message['title'] = json['user']['screen_name']
+        message['text'] = json['text']
+        message['remote_id'] = json['id']
+        message['source'] = 'twitter'
+        message['type'] = 'mention'
+
+        # save the raw message
+        message['raw'] = json
+        result = mongo.post_message(message)
+        return 'Success'
 
     exposed = True
 
@@ -52,7 +73,7 @@ class Donations(object):
             if item['user']:
                 message['title'] = item['user']['full_name']
             else:
-			    message['title'] = "Anonymous Donator"
+                message['title'] = "Anonymous Donator"
             message['text'] = item['project']['title']
             message['remote_id'] = item['id']
             message['source'] = 'onepercentsite'
@@ -79,17 +100,22 @@ class Donations(object):
 
 if __name__ == '__main__':
     cherrypy.tree.mount(
-	    Messages(), '/api/messages/',
-	    {'/':
+        Messages(), '/api/messages/',
+        {'/':
             {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
         }
-	)
+    )
     cherrypy.tree.mount(
-	    Donations(), '/api/donations/',
-	    {'/':
+        TwitterMentions(), '/api/twitter-mentions/',
+        {'/':
             {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
         }
-	)
+    )
+    cherrypy.tree.mount(
+        Donations(), '/api/donations/',
+        {'/':
+            {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
+        }
+    )
     cherrypy.engine.start()
     cherrypy.engine.block()
-
